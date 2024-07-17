64b20767f7928a6487933f008066e60159801071
Timothy Gillespie <timothy@gillespie.eu>
2024-07-17 02:26:12 +0200

    Rename replyId to id for deletions too for consistency

--- src/main/kotlin/io/beatmaps/ws/routes/replies.kt
+++ src/main/kotlin/io/beatmaps/ws/routes/replies.kt
@@ -17,7 +17,7 @@ import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.transactions.transaction
 
 @Serializable
-data class ReplyDeleteDTO(val replyId: Int)
+data class ReplyDeleteDTO(val id: Int)
 
 @Serializable
 data class ReplyWebsocketDTO(
